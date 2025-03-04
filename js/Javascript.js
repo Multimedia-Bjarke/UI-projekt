00368a6447ee40324d727e0010d6587c34c32a57
Multimedia-Bjarke <bjarkedtholm@gmail.com>
2025-03-04 11:06:34 +0100

    .

--- js/Javascript.js
+++ js/Javascript.js
@@ -163,3 +163,337 @@ function SegmenterPris() {
   </div>
   `;
 }
+
+
+function SegmenterPris2() {
+  document.getElementById("productpage").innerHTML = `
+  <div id="product6">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer6_asahi 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Asahi | 33cl</h2>
+      <p class="productprice">100, - dkk</p>
+  </div>
+
+  <div id="product5">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon5">
+                      <img src="images/webshop/products/beer5_jokerAF 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">JokerAF | 0,5l</h2>
+      <p class="productprice">90, - dkk</p>
+  </div>
+
+  <div id="product9">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon9">
+                      <img src="images/webshop/products/beer9_aslan 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">HOP WATER | 33cl</h2>
+      <p class="productprice">75, - dkk</p>
+  </div>
+
+  <div id="product7">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon7">
+                      <img src="images/webshop/products/beer7_noDa 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">HOP Ø | 33cl</h2>
+      <p class="productprice">60, - dkk</p>
+  </div>
+
+  <div id="product3">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer3_appletude.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Appeltude | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+
+  <div id="product2">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer2_zest.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Zest | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+
+  <div id="product1">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer1_berryBloom.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Berry Bloom | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+
+  <div id="product8">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer8_omniPollo 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">OmniPollo | 33cl</h2>
+      <p class="productprice">45, - dkk</p>
+  </div>
+
+  <div id="product4">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon4">
+                      <img src="images/webshop/products/beer4_bridgeRoad 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Free Time | 33cl</h2>
+      <p class="productprice">25, - dkk</p>
+  </div>
+  `;
+}
+
+
+function SegmenterAlfa() {
+  document.getElementById("productpage").innerHTML = `
+  <div id="product3">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer3_appletude.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Appeltude | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+
+  <div id="product6">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer6_asahi 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Asahi | 33cl</h2>
+      <p class="productprice">100, - dkk</p>
+  </div>
+
+  <div id="product1">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer1_berryBloom.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Berry Bloom | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+
+  <div id="product4">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon4">
+                      <img src="images/webshop/products/beer4_bridgeRoad 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Free Time | 33cl</h2>
+      <p class="productprice">25, - dkk</p>
+  </div>
+
+  <div id="product7">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon7">
+                      <img src="images/webshop/products/beer7_noDa 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">HOP Ø | 33cl</h2>
+      <p class="productprice">60, - dkk</p>
+  </div>
+
+  <div id="product9">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon9">
+                      <img src="images/webshop/products/beer9_aslan 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">HOP WATER | 33cl</h2>
+      <p class="productprice">75, - dkk</p>
+  </div>
+
+  <div id="product5">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon5">
+                      <img src="images/webshop/products/beer5_jokerAF 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">JokerAF | 0,5l</h2>
+      <p class="productprice">90, - dkk</p>
+  </div>
+
+  <div id="product8">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer8_omniPollo 1.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">OmniPollo | 33cl</h2>
+      <p class="productprice">45, - dkk</p>
+  </div>
+
+  <div id="product2">
+      <div id="Innerbox1">
+          <div id="product1c">
+              <div class="Innerbox2">
+                  <div class="imagecon">
+                      <img src="images/webshop/products/beer2_zest.png" alt="" />
+                  </div>
+              </div>
+              <div class="productbuttons">
+                  <div class="productread"><a href="">Læs Mere</a></div>
+                  <div class="productbatch"><a href="">Læg i Ølkurv</a></div>
+              </div>
+          </div>
+      </div>
+      <h2 class="productheadline">Zest | 33cl</h2>
+      <p class="productprice">50, - dkk</p>
+  </div>
+  `;
+}
